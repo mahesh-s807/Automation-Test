e3f6a726ea0753ccbc8f5d5b16c73971f338d699
mahesh-s807 <81277657+mahesh-s807@users.noreply.github.com>
2021-05-08 22:26:10 +0530

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,10 @@
+package com.qa.constants;
+
+public class Constant {
+
+	public static final String appUrl="http://demo.guru99.com/test/newtours";
+	public static final String homeTitle="Login: Mercury Tours";
+	public static final int timeout=60;
+
+
+}
